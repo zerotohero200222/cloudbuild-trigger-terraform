e12ee284e28cd4054399f53bf548efe8527502e3
zerotohero200222 <aebharath17@gmail.com>
2025-11-24 19:07:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,32 @@
-# outputs.tf
-
 output "trigger_id" {
-  description = "ID of the Cloud Build trigger"
-  value       = google_cloudbuild_trigger.csr_trigger.id
+  description = "Cloud Build Trigger ID"
+  value       = google_cloudbuild_trigger.github_trigger.id
 }
 
 output "trigger_name" {
-  description = "Name of the trigger"
-  value       = google_cloudbuild_trigger.csr_trigger.name
+  description = "Trigger Name"
+  value       = google_cloudbuild_trigger.github_trigger.name
 }
 
 output "repository" {
-  description = "CSR repository name"
+  description = "GitHub repository used"
   value       = var.repository_name
 }
 
-output "service_account" {
-  description = "Service account used by Cloud Build"
-  value       = var.service_account_email
-}
-
 output "environment" {
-  description = "Environment this trigger belongs to"
+  description = "Environment for the trigger"
   value       = var.environment
 }
 
 output "branch_pattern" {
-  description = "Branch to trigger on"
+  description = "Branch regex used for trigger"
   value       = var.branch_pattern
 }
 
 output "trigger_console_url" {
-  description = "URL to view trigger in GCP Console"
-  value       = "https://console.cloud.google.com/cloud-build/triggers;region=${var.region}/${google_cloudbuild_trigger.csr_trigger.trigger_id}?project=${var.project_id}"
+  value = "https://console.cloud.google.com/cloud-build/triggers;region=${var.region}/${google_cloudbuild_trigger.github_trigger.trigger_id}?project=${var.project_id}"
 }
 
 output "builds_console_url" {
-  description = "URL to view builds in GCP Console"
-  value       = "https://console.cloud.google.com/cloud-build/builds?project=${var.project_id}"
+  value = "https://console.cloud.google.com/cloud-build/builds?project=${var.project_id}"
 }
